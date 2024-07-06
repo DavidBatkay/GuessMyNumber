9622e70bfe2310552188fc019899ff3f173ca970
DavidBatkay <david.batkay@emanuel.ro>
2024-07-06 13:08:30 +0300

    first commit

---
+++ starter/script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (score > 0) {
+    if (!guess) {
+      displayMessage('😢No number!');
+    } else if (guess < 1 || guess > 20) {
+      displayMessage('🙄Number must be between 1 and 20');
+    } else if (guess === secretNumber) {
+      displayMessage('🎉Correct Number!');
+      document.querySelector('body').style.backgroundColor = '#60b347';
+      document.querySelector('.number').style.width = '30rem';
+      document.querySelector('.number').textContent = secretNumber;
+      if (score > highscore) {
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+      }
+    } else if (guess !== secretNumber) {
+      displayMessage(guess > secretNumber ? '📈Too high!' : '📉Too low!');
+      score--;
+    }
+    document.querySelector('.score').textContent = score;
+  }
+  if (score === 0) {
+    displayMessage('😭You lost the game!');
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  displayMessage('Start guessing...');
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+});
